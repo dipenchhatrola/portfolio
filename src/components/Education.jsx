ac3c80b250a4768fe72eb9ed803828a4368f6190
DIPEN CHHATROLA <114484960+dipenchhatrola@users.noreply.github.com>
2024-12-06 09:21:31 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,7 +7,7 @@ function EducationSection() {
                 <div className="row">
                     <h2 className="mb-4">Education</h2>
                     <div className="col-md-6 d-flex flex-column justify-content-center">
-                        <h4 className="text-center text-primary">Atmiya University</h4>
+                        <h4 className="text-start text-primary">Atmiya University</h4>
                         <h5 className="text-start">M.Sc.IT-Master of Science in Information Technology</h5>
                         <h6 className="text-start">2022-2024</h6>
                         <h6 className="mb-5 text-start">CGPA-7.08/10</h6>
@@ -16,13 +16,13 @@ function EducationSection() {
                         <h6 className="mb-5 text-start">CGPA-6.19/10</h6>                            
                     </div>
                     <div className="col-md-6 d-flex flex-column justify-content-center">
-                        <h4 className="text-center text-primary">Shree GK Dholakiya School</h4>
-                        <h5 className="text-center">HSC</h5>
-                        <h6 className="text-center">2019</h6>
-                        <h6 className="mb-5 text-center">70.20 PR</h6>
-                        <h5 className="text-center">SSC</h5>
-                        <h6 className="text-center">2017</h6>
-                        <h6 className="mb-5 text-center">65.20 PR</h6>                            
+                        <h4 className="text-start text-primary">Shree GK Dholakiya School</h4>
+                        <h5 className="text-start">HSC</h5>
+                        <h6 className="text-start">2019</h6>
+                        <h6 className="mb-5 text-start">70.20 PR</h6>
+                        <h5 className="text-start">SSC</h5>
+                        <h6 className="text-start">2017</h6>
+                        <h6 className="mb-5 text-start">65.20 PR</h6>                            
                     </div>
                 </div>
             </div>
