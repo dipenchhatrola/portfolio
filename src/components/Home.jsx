60aa9702e83992bbbea3aea72f5a91cac86e4582
DIPEN CHHATROLA <114484960+dipenchhatrola@users.noreply.github.com>
2024-12-17 13:04:18 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ViewMyWorkBtn from "./ViewMyWorkBtn";
 import HeaderImg from "../assets/images/headerimg.jpeg"; 
 
 function Home() {
@@ -9,8 +8,10 @@ function Home() {
                 <div className="row">
                     <div className="col-md-6 d-flex flex-column align-items-start justify-content-center mt-5 mt-md-0">
                         <h2 className="text-secondary fw-bold lh-1">DIPEN CHHATROLA</h2>
-                        <h3 className="text-capitalize text-start text-primary lh-1 mb-3">Web Devloper</h3>
-                        <ViewMyWorkBtn />
+                        <h3 className="text-capitalize text-start text-primary lh-1 mb-3">Web Developer</h3>
+                        <div className="d-flex align-items-center justify-content-center justify-content-lg-start pt-5 ml-4">
+                            <a href="/Dipen_Chhatrola.pdf" className="btn btn-primary mr-4" aria-label="Download Dipen Patel's Resume">Download Resume</a>
+                        </div>
                     </div>
                     <div className="col-md-6 d-flex justify-content-center justify-content-md-end">
                         <img className="img-fluid w-75 rounded-circle shadow my-5" src={HeaderImg} alt="header img" />
