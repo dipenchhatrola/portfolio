4547b918e95958fb77e0b88fe41fcd3011c39ccc
DIPEN CHHATROLA <114484960+dipenchhatrola@users.noreply.github.com>
2024-12-17 13:03:33 +0530

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,11 +7,10 @@ function NavigationaBar() {
     return (
         <Navbar expand="lg" className="bg-primary w-100 fixed-top z-1" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand className="fs-4">Dipen <span className="text-light">A. Chhatrola</span></Navbar.Brand>
+                <Navbar.Brand className="fs-4" href="#home">Dipen <span className="text-light">A. Chhatrola</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto w-100 d-flex justify-content-end">
-                        <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#education">Education</Nav.Link>
                         <Nav.Link href="#skill">Experience and Skill</Nav.Link>
