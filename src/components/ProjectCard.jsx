955a9f9377e86321cb4f5a5c3b749469e84b607e
DIPEN CHHATROLA <114484960+dipenchhatrola@users.noreply.github.com>
2025-05-31 13:36:19 +0530

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,7 +9,7 @@ function ProjectCard(props) {
                 <Card.Img variant="top" src={props.projectImage} />
                 <Card.Body className="p-5">
                     <Card.Title className="fw-bold text-uppercase mb-4 fs-3">{props.projectName}</Card.Title>
-                    <Button className="shadow text-capitalize" variant="outline-primary" onClick="ViewProject()">View Project</Button>
+                    <Button className="shadow text-capitalize" variant="outline-primary">View Project</Button>
                 </Card.Body>
             </Card>
         </div>
